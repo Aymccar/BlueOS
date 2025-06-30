bcf78cb038fc59e2a6db9bac86a4ffc04a6e6ece
Aymeric <aymericardot@gmail.com>
2025-06-30 15:26:24 +0200

    Bridge path modification

--- core/tools/wafros2bridge.sh
+++ core/tools/wafros2bridge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 apt install git python3-colcon-common-extensions build-essential -y
-cd /
-git clone -b dev --single-branch https://github.com/Aymccar/Waf_Ros2_Bridge.git
+mkdir -p /colcon_ws/src
+git clone -b dev --single-branch https://github.com/Aymccar/Waf_Ros2_Bridge.git /colcon_ws/src/Waf_Ros2_Bridge
 . /opt/ros/jazzy/setup.sh
-cd Waf_Ros2_Bridge/colcon_ws && colcon build
-echo "$(pwd)/install/wafros2bridge/lib/" > /etc/ld.so.conf.d/wafros2bridge.conf
+cd /colcon_ws && colcon build
+echo "/colcon_ws/install/wafros2bridge/lib/" > /etc/ld.so.conf.d/wafros2bridge.conf
 ldconfig
 printf "ros2 for waf bridge installed"
